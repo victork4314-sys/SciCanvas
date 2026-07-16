c4226217cce9c1e52a7b04e6c105945ee1dc5ba1
victork4314-sys <victork4314@gmail.com>
2026-07-16 22:48:04 +0200

    Add cloud backend configuration

---
+++ cloud-config.js
@@ -0,0 +1,6 @@
+window.SCICANVAS_CLOUD_CONFIG = window.SCICANVAS_CLOUD_CONFIG || {
+  supabaseUrl: '',
+  supabaseAnonKey: '',
+  redirectUrl: `${window.location.origin}${window.location.pathname}`,
+  appName: 'SciCanvas'
+};
