0d2ab86879b91ff779c35876d040ca2aa08fd80a
victork4314-sys <victork4314@gmail.com>
2026-07-18 14:16:59 +0200

    Advance wrapped text cache build

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const FIGURELOOM_BUILD_ID = "chunk-25-text-layout-20260718-v1";
+const FIGURELOOM_BUILD_ID = "chunk-25-text-layout-20260718-v2";
 const FIGURELOOM_CACHE_PREFIX = "figureloom-app-";
 const FIGURELOOM_CACHE_NAME = `${FIGURELOOM_CACHE_PREFIX}${FIGURELOOM_BUILD_ID}`;
 const LEGACY_CACHE_PREFIXES = ["figureloom-shell", "scicanvas-shell"];
