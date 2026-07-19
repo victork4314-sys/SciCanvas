8b400a800a8962723cda8a00abcd81c1fbea363b
victork4314-sys <victork4314@gmail.com>
2026-07-19 14:02:48 +0200

    Cache the unified interface theme

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const FIGURELOOM_BUILD_ID = "stable-71d36df-locked-20260719-v38";
+const FIGURELOOM_BUILD_ID = "stable-71d36df-locked-20260719-v39";
 const FIGURELOOM_CACHE_PREFIX = "figureloom-app-";
 const FIGURELOOM_CACHE_NAME = `${FIGURELOOM_CACHE_PREFIX}${FIGURELOOM_BUILD_ID}`;
 const LEGACY_CACHE_PREFIXES = ["figureloom-shell", "scicanvas-shell"];
@@ -24,6 +24,7 @@ self.addEventListener("install", event => {
       cache.add(new Request("./tour-mobile-safe.js", { cache:"reload" })),
       cache.add(new Request("./visible-brand-finalizer.js", { cache:"reload" })),
       cache.add(new Request("./help-center.js", { cache:"reload" })),
+      cache.add(new Request("./figureloom-sage-theme.js", { cache:"reload" })),
       cache.add(new Request("./wiki/", { cache:"reload" })),
       cache.add(new Request("./wiki/index.html", { cache:"reload" })),
       cache.add(new Request("./wiki/wiki.css", { cache:"reload" })),
