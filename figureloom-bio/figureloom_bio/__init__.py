c8b8046662dff7e57104ecee5a7dd403ee3fb853
victork4314-sys <victork4314@gmail.com>
2026-07-22 21:01:19 +0200

    Load the platform expansion

--- figureloom-bio/figureloom_bio/__init__.py
+++ figureloom-bio/figureloom_bio/__init__.py
@@ -4,11 +4,13 @@ from .runtime import Runner
 from .runtime_extensions import install_runtime_extensions
 from .runtime_repeat_reset import install_repeat_reset
 from .sequence_management import install_sequence_management
+from .platform_expansion import install_platform_expansion
 
 install_runtime_extensions(Runner)
 install_repeat_reset(Runner)
 install_sequence_management(Runner)
+install_platform_expansion(Runner)
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 __all__ = ["Runner", "__version__"]
