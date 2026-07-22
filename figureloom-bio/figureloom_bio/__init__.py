d19c6f227c6d81ce6f69bf2987a5be87fa664c93
victork4314-sys <victork4314@gmail.com>
2026-07-22 19:48:33 +0200

    Reset paired state between repetitions

--- figureloom-bio/figureloom_bio/__init__.py
+++ figureloom-bio/figureloom_bio/__init__.py
@@ -2,8 +2,10 @@
 
 from .runtime import Runner
 from .runtime_extensions import install_runtime_extensions
+from .runtime_repeat_reset import install_repeat_reset
 
 install_runtime_extensions(Runner)
+install_repeat_reset(Runner)
 
 __version__ = "0.1.0"
 
