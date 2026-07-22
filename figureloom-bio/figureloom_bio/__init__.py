0152b66dec7410c003d24a8bd08830578fd1b57d
victork4314-sys <victork4314@gmail.com>
2026-07-22 22:41:00 +0200

    Load FigureLoom Bio add-on packages

--- figureloom-bio/figureloom_bio/__init__.py
+++ figureloom-bio/figureloom_bio/__init__.py
@@ -7,6 +7,7 @@ from .sequence_management import install_sequence_management
 from .genomics_core import install_genomics_core
 from .unique_name_fix import install_unique_name_fix
 from .workflow_expansion import install_workflow_expansion
+from .addon_packages import install_addon_packages
 
 install_runtime_extensions(Runner)
 install_repeat_reset(Runner)
@@ -14,7 +15,8 @@ install_sequence_management(Runner)
 install_genomics_core(Runner)
 install_unique_name_fix(Runner)
 install_workflow_expansion(Runner)
+install_addon_packages(Runner)
 
-__version__ = "0.4.0"
+__version__ = "0.5.0"
 
 __all__ = ["Runner", "__version__"]
