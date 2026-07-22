08113f85d2c836627bb0e837e5453515c9ddd13b
victork4314-sys <victork4314@gmail.com>
2026-07-22 20:11:08 +0200

    Load the sequence organization commands

--- figureloom-bio/figureloom_bio/__init__.py
+++ figureloom-bio/figureloom_bio/__init__.py
@@ -7,6 +7,8 @@ from .runtime_repeat_reset import install_repeat_reset
 install_runtime_extensions(Runner)
 install_repeat_reset(Runner)
 
-__version__ = "0.1.0"
+from . import sequence_expansion as _sequence_expansion  # noqa: E402,F401
+
+__version__ = "0.2.0"
 
 __all__ = ["Runner", "__version__"]
