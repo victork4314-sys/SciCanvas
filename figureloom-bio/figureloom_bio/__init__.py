11faf5fdb06e559f11537320656cabd597e4a1cd
victork4314-sys <victork4314@gmail.com>
2026-07-22 21:30:24 +0200

    Load official translator compatibility rules

--- figureloom-bio/figureloom_bio/__init__.py
+++ figureloom-bio/figureloom_bio/__init__.py
@@ -5,11 +5,13 @@ from .runtime_extensions import install_runtime_extensions
 from .runtime_repeat_reset import install_repeat_reset
 from .sequence_management import install_sequence_management
 from .platform_expansion import install_platform_expansion
+from .translator_compat import install_translator_compat
 
 install_runtime_extensions(Runner)
 install_repeat_reset(Runner)
 install_sequence_management(Runner)
 install_platform_expansion(Runner)
+install_translator_compat()
 
 __version__ = "0.3.0"
 
