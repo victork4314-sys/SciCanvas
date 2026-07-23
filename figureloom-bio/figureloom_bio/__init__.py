010a598c385c968e57f26ed53e1156c0bc4993c2
victork4314-sys <victork4314@gmail.com>
2026-07-24 00:47:12 +0200

    Install final language recovery and volcano runtime

--- figureloom-bio/figureloom_bio/__init__.py
+++ figureloom-bio/figureloom_bio/__init__.py
@@ -22,6 +22,7 @@ from .paired_length_parity import install_paired_length_parity
 from .language_label_parity import install_language_label_parity
 from .language_source_parity import install_language_source_parity
 from .language_execution_parity import install_language_execution_parity
+from .final_language_science import install_final_language_science
 
 install_runtime_extensions(Runner)
 install_repeat_reset(Runner)
@@ -44,6 +45,7 @@ install_paired_length_parity(Runner)
 install_language_label_parity()
 install_language_source_parity()
 install_language_execution_parity()
+install_final_language_science(Runner)
 
 __version__ = "0.8.0"
 
