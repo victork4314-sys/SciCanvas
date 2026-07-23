90ffaa4309e9ba9cfde82c3d05de01dabdb9835b
victork4314-sys <victork4314@gmail.com>
2026-07-23 22:49:38 +0200

    Use exact web syntax layers in native builds

--- figureloom-bio/platform/ide_entry.py
+++ figureloom-bio/platform/ide_entry.py
@@ -1,11 +1,13 @@
 from figureloom_bio import native_account, native_ide
 from figureloom_bio.native_account_runtime import install_runtime_fixes
+from figureloom_bio.native_syntax_web_exact import install_exact_web_syntax
 from figureloom_bio.native_web_parity import install_web_parity
 
 
 install_runtime_fixes(native_account)
 native_account.install_native_account(native_ide)
 install_web_parity(native_ide)
+install_exact_web_syntax()
 
 
 if __name__ == "__main__":
