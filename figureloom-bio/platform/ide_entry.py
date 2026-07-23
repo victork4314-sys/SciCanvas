7388e7b8f831c84a67fdb41141b7df8b10c383ad
victork4314-sys <victork4314@gmail.com>
2026-07-23 23:40:22 +0200

    Install native startup safety and crash reporting

--- figureloom-bio/platform/ide_entry.py
+++ figureloom-bio/platform/ide_entry.py
@@ -1,5 +1,6 @@
 from figureloom_bio import native_account, native_ide
 from figureloom_bio.native_account_runtime import install_runtime_fixes
+from figureloom_bio.native_stability import install_native_stability, run_stable_ide
 from figureloom_bio.native_syntax_web_exact import install_exact_web_syntax
 from figureloom_bio.native_web_parity import install_web_parity
 
@@ -8,7 +9,8 @@ install_runtime_fixes(native_account)
 native_account.install_native_account(native_ide)
 install_web_parity(native_ide)
 install_exact_web_syntax()
+install_native_stability(native_ide)
 
 
 if __name__ == "__main__":
-    raise SystemExit(native_ide.run_native_ide())
+    raise SystemExit(run_stable_ide(native_ide))
