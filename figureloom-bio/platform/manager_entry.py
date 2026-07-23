32f21ce49c8990b450620de276f51e0b4cde73d1
victork4314-sys <victork4314@gmail.com>
2026-07-24 00:43:31 +0200

    Install verified updater reliability after painted-window safety

--- figureloom-bio/platform/manager_entry.py
+++ figureloom-bio/platform/manager_entry.py
@@ -6,10 +6,12 @@ if "--self-test" in sys.argv:
     os.environ.setdefault("QT_QPA_PLATFORM", "offscreen")
 
 from figureloom_bio import platform_qt_tools
+from figureloom_bio.desktop_reliability import install_desktop_tool_reliability
 from figureloom_bio.platform_tool_safety import install_platform_tool_safety
 
 
 install_platform_tool_safety(platform_qt_tools)
+install_desktop_tool_reliability(platform_qt_tools)
 
 
 if __name__ == "__main__":
