fb89bdc34906d0b61997ed49a1e4cbb8ba2ba8e0
victork4314-sys <victork4314@gmail.com>
2026-07-24 00:21:20 +0200

    Install updater reliability before opening the manager

--- figureloom-bio/platform/manager_entry.py
+++ figureloom-bio/platform/manager_entry.py
@@ -1,5 +1,9 @@
-from figureloom_bio.platform_qt_tools import show_manager_window
+from figureloom_bio import platform_qt_tools
+from figureloom_bio.desktop_reliability import install_desktop_tool_reliability
+
+
+install_desktop_tool_reliability(platform_qt_tools)
 
 
 if __name__ == "__main__":
-    raise SystemExit(show_manager_window())
+    raise SystemExit(platform_qt_tools.show_manager_window())
