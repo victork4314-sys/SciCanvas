a0ae7604981a58cb5aad2f4057c75d2fcc65d8df
victork4314-sys <victork4314@gmail.com>
2026-07-24 00:09:07 +0200

    Release the updater after safe installer handoff

--- figureloom-bio/platform/manager_entry.py
+++ figureloom-bio/platform/manager_entry.py
@@ -1,8 +1,10 @@
 from figureloom_bio import platform_qt
+from figureloom_bio.desktop_reliability import install_updater_handoff
 from figureloom_bio.platform_qt_guard import install_platform_qt_guard
 
 
 install_platform_qt_guard(platform_qt)
+install_updater_handoff(platform_qt)
 
 
 if __name__ == "__main__":
