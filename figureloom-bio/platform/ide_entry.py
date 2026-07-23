e15511762946e415f34d9e41b182c8b9de53e43f
victork4314-sys <victork4314@gmail.com>
2026-07-24 00:43:57 +0200

    Add large-result startup safety and visible figure previews

--- figureloom-bio/platform/ide_entry.py
+++ figureloom-bio/platform/ide_entry.py
@@ -1,6 +1,7 @@
 from figureloom_bio import native_account, native_ide, native_widgets
 from figureloom_bio.native_account_runtime import install_runtime_fixes
 from figureloom_bio.native_run_safety import install_native_run_safety
+from figureloom_bio.native_stability import install_native_stability, run_stable_ide
 from figureloom_bio.native_syntax_web_exact import install_exact_web_syntax
 from figureloom_bio.native_web_parity import install_web_parity
 
@@ -18,7 +19,8 @@ native_widgets.LIGHT.setdefault("panel_2", native_widgets.LIGHT["editor_gutter"]
 native_widgets.DARK.setdefault("panel_2", native_widgets.DARK["editor_gutter"])
 
 install_exact_web_syntax()
+install_native_stability(native_ide)
 
 
 if __name__ == "__main__":
-    raise SystemExit(native_ide.run_native_ide())
+    raise SystemExit(run_stable_ide(native_ide))
